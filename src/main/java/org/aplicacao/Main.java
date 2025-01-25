dc59a7e4af0a1d6a53683aca7a913db20e8717e7
Lucas Paz <lucaspaz283@gmail.com>
2025-01-25 09:51:56 -0300

    Created Server

--- src/main/java/org/aplicacao/Main.java
+++ src/main/java/org/aplicacao/Main.java
@@ -1,7 +1,19 @@
 package org.aplicacao;
 
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws IOException {
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+
+        server.createContext("/categorias", new ProdutoController());
+
+        server.setExecutor(null);
+        server.start();
+        System.out.println("Servidor rodando na porta 8080");
     }
 }
